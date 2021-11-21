b186ffe917168d2ad420e36f389189e73ddf8880
Anurag-dws <anurag.kushwaha@isobar.com>
2021-11-21 09:34:23 +0530

    dynamic port binding added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const hbs = require('express-handlebars');
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 5000;
 const path = require("path");
 var menu = require("./routes/menu");
 
